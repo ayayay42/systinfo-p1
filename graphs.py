22bdf7e47427b6223d638c3578e52b4154156578
Nouha El hariri <80458621+xonouhaxo@users.noreply.github.com>
2023-12-10 21:40:33 +0100

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -1,37 +1,42 @@
-import pandas as pd
+import csv
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
-try:
-    import seaborn as sns #pour de plus beaux graph askip 
-except ModuleNotFoundError:
-    print("Seaborn not found")
-else:
-    sns.set()
 
-#program_names = ["philosophes", "philosophes-my-functions", "thread_test"] #noms des programmes à évaluer
-program_names = ["readerWriter"] #TODO: ajouter tous les programmes à plot 
-df = pd.read_csv("perf_measures.csv")
-#df["time"] *= 1000 #in ms 
+with open('perf_measures.csv', newline='') as csvfile:
+    data = list(csv.DictReader(csvfile))
 
-for i in range(len(program_names)):
-	current = program_names[i] #nom du programme courant
-	plt.figure(figsize=(10, 5))
-	plt.xlabel("nb thread")
-	#plt.xscale('log', base = 2)
-	#plt.xlim(left=1, right=68)
-	plt.ylabel("temps en s")
 
-	#peut-être plus faire une boucle sur les programmes et faire un subplot pour chaque programme pour mieux s'y retrouver et que y ai le bon nom
-	plt.title("Eval perf pour le programme: " + current) 
+programs = {}
+for row in data:
+    program_name = row['program']
+    category = program_name.split('-')[0] 
 
-	current_df = df[df["program"] == current]
-	grouped_data = current_df[['num_threads', 'time']].groupby("num_threads").agg(['mean', 'std']) 
-	print(grouped_data)
-	print(grouped_data['time']['mean'])
-	print(grouped_data['time']['std'])
+    if category not in programs:
+        programs[category] = {}
 
-	plt.errorbar(grouped_data.index, grouped_data['time']['mean'], yerr=grouped_data['time']['std'], fmt='-o')
-	#TODO: ajouter l'errorbar de nos implémentations sur le même graph que l'original (donc 3 lignes)
-	max_val = max(grouped_data['time']['mean'])
-	plt.ylim(bottom=0, top=max_val*1.2)
-	plt.savefig("plots/" + current + ".png")
+    if program_name not in programs[category]:
+        programs[category][program_name] = {'threads': [], 'time': []}
+
+    programs[category][program_name]['threads'].append(int(row['num_threads']))
+    programs[category][program_name]['time'].append(float(row['time']))
+
+#Plotting
+fig, axs = plt.subplots(len(programs), 1, figsize=(10, 6 * len(programs)), sharex=True)
+
+for i, (category, program_data) in enumerate(programs.items()):
+    ax = axs[i]
+
+    for program_name, results in program_data.items():
+        avg_time = np.mean(results['time'])
+        std_dev = np.std(results['time'])
+
+        ax.errorbar(results['threads'], results['time'], yerr=std_dev, fmt='o-', label=f'{program_name} (Avg: {avg_time:.2f} s)')
+
+    ax.set_title(f'Category: {category}')
+    ax.set_ylabel('Execution Time (s)')
+    ax.legend()
+    ax.grid(True)
+
+plt.xlabel('Number of Threads')
+plt.suptitle('Execution Time vs Number of Threads (Grouped by Category)', y=0.92, fontsize=16)
+plt.show()
